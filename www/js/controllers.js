67d7214dcb5be7260697a4da0cf5f273b826400f
Psarmmiey <psarmmiey@gmail.com>
2016-08-25 00:43:18 +0100

    Fiddling continues

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,47 +1,53 @@
 var bibleCtrl = angular.module('starter.controllers', []);
 
-bibleCtrl.controller('DashCtrl', function ($scope, $http, $state, $rootScope, BibleService, $stateParams) {
-    $scope.getData = function () {
-        BibleService.getAllBooks().then(function (res) {
-            $scope.books = res.data;
-        }), function (err) {
-            alert(err);
-        };
+bibleCtrl.controller('DashCtrl', function($scope, $http, $state, $rootScope, BibleService, $stateParams) {
+    $scope.getData = function() {
+        BibleService.getAllBooks().then(function(res) {
+                $scope.books = res.data;
+            }),
+            function(err) {
+                alert(err);
+            };
     };
-    
+
     $scope.getData();
 
 
-    $scope.goToChapter = function (bk) {
+    $scope.goToChapter = function(bk) {
         $rootScope.chapters = bk.chapters;
         console.log(bk.chapters);
 
     };
 
-    $scope.goVerse = function (chapter, chapNum)
-    {
+    $scope.goVerse = function(chapter, chapNum) {
         //$state.go('tab.verse', {"chapter": chapter});
         $rootScope.verses = chapter[chapNum];
         console.log($rootScope.verses);
     };
-    
+
 });
 
-bibleCtrl.controller('ChatsCtrl', function ($scope, Chats) {
+bibleCtrl.controller('ChatsCtrl', function($scope, Chats) {
 
     $scope.chats = Chats.all();
-    $scope.remove = function (chat) {
+    $scope.remove = function(chat) {
         Chats.remove(chat);
     };
 })
 
-bibleCtrl.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+bibleCtrl.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
-    $scope.find = function () {
+    $scope.find = function() {
         alert("finding verses");
     }
 })
 
-bibleCtrl.controller('VerseCtrl', function ($scope, $stateParams) {
+bibleCtrl.controller('VerseCtrl', function($scope, $stateParams) {
     alert($stateParams.chapter);
 });
+
+function ContentController($scope, $ionicSideMenuDelegate) {
+    $scope.toggleRight = function() {
+        $ionicSideMenuDelegate.toggleRight();
+    };
+}
